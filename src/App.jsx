7252a91f943cd6c6ab27730c1b1f57f9eaa1cfc6
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-11-18 15:55:38 +0100

    Usestate function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
+
 function App() {
 
-    function handleClick() {
-        alert("klik!")
+    const [isSpoilerShown, setIsSpoilerShown] = useState(false);
+    const [isWarningShown, setIsWarningShown] = useState(true)
+
+    function handleShowSpoiler() {
+        setIsSpoilerShown(true);
+        setIsWarningShown(false);
+    }
+
+    function handleCloseWarning() {
+        setIsWarningShown(false)
     }
 
     return (
         <>
             <h1>Gwiezdne wojny</h1>
+
             <h2>Rok produkcji: 1980</h2>
-            <p>Zli atakują dobrych. Potrzebna pomoc</p>
-            <button onClick={handleClick}>Spoiler</button>
+
+            <h2>Fabuła</h2>
+
+            {isWarningShown && (<p>Uwaga! Opis zawiera spoiler! <button onClick={handleCloseWarning}>X</button></p>)}
+
+            <p>Źli atakują dobrych. Potrzebna pomoc.</p>
+
+            {isSpoilerShown ? (<p>Vader jest ojcem Luke.</p>) : (
+                <button onClick={handleShowSpoiler}>Uważaj na spoiler</button>
+            )}
         </>
     )
 }
