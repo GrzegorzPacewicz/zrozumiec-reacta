be2f161f44dce121f3c09fa35d90257b3308eb3c
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-11-18 15:10:55 +0100

    Strict mode removed. Button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 function App() {
 
-  return (
-   <>
-   <div>Hello world!</div>
-   </>
-  )
+    function handleClick() {
+        alert("klik!")
+    }
+
+    return (
+        <>
+            <h1>Gwiezdne wojny</h1>
+            <h2>Rok produkcji: 1980</h2>
+            <p>Zli atakują dobrych. Potrzebna pomoc</p>
+            <button onClick={handleClick}>Spoiler</button>
+        </>
+    )
 }
 
 export default App
