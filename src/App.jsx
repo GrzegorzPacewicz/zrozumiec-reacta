d9cebe6257d89a6881a356a465c3062acbdca492
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-12-02 23:14:24 +0100

    React Router installed. Home added. FilmWeb.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,17 @@
-import { LikesCounter } from "./components/LikesCounter.jsx";
-import { Plot } from "./components/Plot.jsx";
-import { ReviewForm } from "./components/ReviewForm.jsx";
-import { Reviews } from "./components/Reviews.jsx";
-import { useState } from "react";
-
-const initialReviews = [
-    { author: "Brian", text: "No!", id: 1 },
-    { author: "Jessica", text: "Yes!", id: 2 },
-]
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FilmWeb } from "./FilmWeb.jsx";
+import { Home } from "./components/Home.jsx";
 
 function App() {
 
-    const [numberOfLikes, setNumberOfLikes] = useState(50);
-    const [reviews, setReviews] = useState(initialReviews)
-    const [counterShown, setCounterShown] = useState(true)
-
-    function handleButtonOfLikesClick() {
-        setNumberOfLikes(previousNumberOfLikes => previousNumberOfLikes +1)
-    }
-
-    function handleButtonLoveIt() {
-        setNumberOfLikes(previousNumberOfLikes => previousNumberOfLikes +3);
-    }
-
-    const handleReviewSubmit = (author, text) =>  {
-        setReviews((prevReviews) => {
-            return [
-                { author, text, id: prevReviews.length + 1 },
-                ...prevReviews,
-            ]
-        });
-    }
-
     return (
         <>
-            <h1>Gwiezdne wojny</h1>
-            <h2>Rok produkcji: 1980</h2>
-          <button onClick={() => {setCounterShown((prevCounterShown) => !prevCounterShown) }}>{counterShown ? "Schowaj counter" : "Pokaż counter" }</button>
-            {counterShown &&   <LikesCounter
-                numberOfLikes={numberOfLikes}
-                onLikeButtonClick={handleButtonOfLikesClick}
-                onLoveButtonClick={handleButtonLoveIt}/>}
-            <Plot/>
-            <Reviews reviews={reviews} />
-            <ReviewForm onReviewSubmit={handleReviewSubmit}/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/filmweb" element={<FilmWeb />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link, Route, Routes } from "react-router-dom";
+
+export function Home() {
+    return (
+        <>
+            <Link to="filmweb">Filmweb</Link>
+        </>
+    );
+}
