86cdb790d17db4ab22ade82a60c3449ee18ad2e9
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-12-03 10:28:15 +0100

    Css file added

--- src/components/DynamicEmojis.jsx
+++ src/components/DynamicEmojis.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { List } from "./List.jsx";
 
-export function DynamicEmojis(props) {
+export function DynamicEmojis() {
     return (
         <>
         <List />
